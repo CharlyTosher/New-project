ade9648c564b82ff1b8c4c69818bf2584ab25e69
CharlyTosher <piratgameplanet@yandex.ru>
2023-05-24 13:43:40 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// Пагинация. Взято с сайта swiperjs.com
+const swiper = new Swiper(".swiper", {
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
+
+  // Не работает почему-то. Пересмотри с момента добавления пагинации.
